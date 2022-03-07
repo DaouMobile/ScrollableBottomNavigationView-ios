4381f1ed472e56612233bfe6af579e771c09feac
gwonii <hc.gwonii@gmail.com>
2022-03-07 12:22:53 +0900

    feat: Sample 프로젝트 추가 및 테스트

--- Tests/ScrollableBottomNavigationView-iosTests/ScrollableBottomNavigationView_iosTests.swift
+++ Tests/ScrollableBottomNavigationView-iosTests/ScrollableBottomNavigationView_iosTests.swift
@@ -6,6 +6,5 @@ final class ScrollableBottomNavigationView_iosTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ScrollableBottomNavigationView_ios().text, "Hello, World!")
     }
 }
