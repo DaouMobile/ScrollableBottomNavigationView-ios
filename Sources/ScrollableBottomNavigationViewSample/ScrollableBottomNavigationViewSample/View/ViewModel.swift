c8695b6e299efceb6788b79134444e103348c0cf
gwonii <hc.gwonii@gmail.com>
2022-03-07 14:39:01 +0900

    feature: ScrollableBottomNavigation을 사용한 샘플 코드 작섣ㅇ
    - scrollableBottomNavigationView spm 임시 develop 브랜치로 변경

---
+++ Sources/ScrollableBottomNavigationViewSample/ScrollableBottomNavigationViewSample/View/ViewModel.swift
@@ -0,0 +1,24 @@
+import Foundation
+import RxSwift
+import RxCocoa
+import ScrollableBottomNavigationView_ios
+
+final class ViewModel {
+    
+    private let _bottomMenuItems: BehaviorRelay<[MenuItem]>
+    var bottomMenuItems: Driver<[MenuItem]> {
+        _bottomMenuItems.asDriver()
+    }
+    
+    init() {
+        _bottomMenuItems = .init(value: [
+            BottomMenuItem(id: "040", name: "투데이"),
+            BottomMenuItem(id: "041", name: "대화"),
+            BottomMenuItem(id: "042", name: "조직도"),
+            BottomMenuItem(id: "043", name: "알림1"),
+            BottomMenuItem(id: "044", name: "알림2"),
+            BottomMenuItem(id: "045", name: "알림3"),
+            BottomMenuItem(id: "046", name: "알림4")
+        ])
+    }
+}
